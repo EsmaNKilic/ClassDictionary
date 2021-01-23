6d43a82dbd9627d3a667d9fb51c47e2cbbcff8cf
Esma <esma-kilic@users.noreply.github.com>
2021-01-23 18:44:00 +0300

    Proje dosyası ekle.

---
+++ ClassDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassDictionary
+{
+    class MyDictionary<T_k, T_v>
+    {
+        T_k[] _key;
+        T_v[] _value;
+        T_k[] _tempkey;
+        T_v[] _tempvalue;
+        public MyDictionary()
+        {
+            _key = new T_k[0];
+            _value = new T_v[0];
+        }
+        public void Add(T_k item, T_v item1)
+        {
+            _tempkey = _key;
+            _key = new T_k[_key.Length + 1];
+            for (int i = 0; i < _tempkey.Length; i++)
+            {
+                _key[i] = _tempkey[i];
+            }
+            _key[_key.Length - 1] = item;
+
+            _tempvalue = _value;
+            _value = new T_v[_value.Length + 1];
+            for (int i = 0; i < _tempvalue.Length; i++)
+            {
+                _value[i] = _tempvalue[i];
+            }
+            _value[_value.Length - 1] = item1;
+        }
+
+        public void Yazdir()
+        {
+            Console.WriteLine("  " + "-----> Eskrim Oyuncuları Ve Tuşlamalar Tablosu <-----\n");
+            for (int i = 0; i < _key.Length; i++)
+            {
+                Console.WriteLine("  " + _key[i] + ": " + _value[i] + " tuş");
+            }
+        }
+    }
+}
---
+++ ClassDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> OyuncularVeTuslari = new MyDictionary<string, int>();
+
+            OyuncularVeTuslari.Add("Esma", 5);
+            OyuncularVeTuslari.Add("Buket", 6);
+            OyuncularVeTuslari.Add("Eda", 5);
+            OyuncularVeTuslari.Add("Zeynep", 3);
+            OyuncularVeTuslari.Add("Kaan", 4);
+
+            OyuncularVeTuslari.Yazdir();
+     
+        }
+    }
+
+    
+}
